1a0988b084caa1f248230df2bc99c51ef54efd49
Leandro Santiago Gomes <leandroluk@gmail.com>
2026-01-21 14:36:12 -0300

    fix: Must impl

--- util/util.go
+++ util/util.go
@@ -4,9 +4,9 @@ package util
 func Ptr[T any](value any) T { return value.(T) }
 
 // Must panics if err is non-nil, otherwise returns the value asserted as type T.
-func Must[T any](value any, err error) T {
+func Must[T any](value T, err error) T {
 	if err != nil {
 		panic(err)
 	}
-	return value.(T)
+	return value
 }

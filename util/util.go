90147597a2cc1f1eee76f6156c32802c370c4a14
Leandro Santiago Gomes <leandroluk@gmail.com>
2026-01-21 15:15:18 -0300

    fix: bug on util.Ptr

--- util/util.go
+++ util/util.go
@@ -1,7 +1,9 @@
 package util
 
 // Ptr returns the value asserted as type T.
-func Ptr[T any](value any) T { return value.(T) }
+func Ptr[T any](v T) *T {
+	return &v
+}
 
 // Must panics if err is non-nil, otherwise returns the value asserted as type T.
 func Must[T any](value T, err error) T {
